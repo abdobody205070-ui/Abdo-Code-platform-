96908286207779802b3eda8c120df7663e24fc83
Abdultif sayed- <abdobody205070@gmail.com>
2026-05-02 19:37:32 +0300

    Add course management functions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+let courses = [];
+
+function addCourse() {
+  let title = document.getElementById("title").value;
+  let link = document.getElementById("link").value;
+
+  if (title === "" || link === "") {
+    alert("Enter all data");
+    return;
+  }
+
+  let course = {
+    title: title,
+    link: link
+  };
+
+  courses.push(course);
+  displayCourses();
+
+  document.getElementById("title").value = "";
+  document.getElementById("link").value = "";
+}
+
+function displayCourses() {
+  let list = document.getElementById("courseList");
+  list.innerHTML = "";
+
+  courses.forEach((c, index) => {
+    list.innerHTML += `
+      <div class="card">
+        <h3>${c.title}</h3>
+        <a href="${c.link}" target="_blank">Watch Course</a>
+      </div>
+    `;
+  });
+}
